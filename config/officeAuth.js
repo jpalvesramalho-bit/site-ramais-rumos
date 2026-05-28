bebbf7dcef341feb0edd436d8749e73c6fefb057
joao alves <jpalvesramalho@gmail.com>
2026-05-28 11:22:39 -0300

    Microsoft 365 login

--- config/officeAuth.js
+++ config/officeAuth.js
@@ -1,9 +1,9 @@
 const redirectUri = process.env.MS_REDIRECT_URI || 'http://localhost:3000/auth/callback';
 
 const officeAuthConfig = {
-    tenantId: process.env.MS_TENANT_ID || 'COLOQUE_SEU_TENANT_ID',
-    clientId: process.env.MS_CLIENT_ID || 'COLOQUE_SEU_CLIENT_ID',
-    clientSecret: process.env.MS_CLIENT_SECRET || 'COLOQUE_SEU_CLIENT_SECRET',
+    tenantId: process.env.MS_TENANT_ID || 'a43b80bf-06e7-47f3-b84f-e718c0d37a71',
+    clientId: process.env.MS_CLIENT_ID || '3df0e69d-c01e-4b50-b0e6-81f4712791b9',
+    clientSecret: process.env.MS_CLIENT_SECRET || ""
     redirectUri,
     postLogoutRedirectUri: process.env.MS_POST_LOGOUT_REDIRECT_URI || new URL(redirectUri).origin,
     scopes: ['openid', 'profile', 'email'],
